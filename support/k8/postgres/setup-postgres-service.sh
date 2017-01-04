3d0ada164ec69e53ba5dc361d4ce0f046cecbefe
Stephen Sisk <sisk@google.com>
2017-01-04 10:11:19 -0800

    Switch to using the container's disk rather than a k8 persistent volume

--- support/k8/postgres/setup-postgres-service.sh
+++ support/k8/postgres/setup-postgres-service.sh
@@ -1,4 +1,4 @@
-./kubectl.sh create -f postgres-persistence.yml
-./kubectl.sh create -f postgres-claim.yml
-./kubectl.sh create -f postgres-pod.yml
+#./kubectl.sh create -f postgres-persistence.yml
+#./kubectl.sh create -f postgres-claim.yml
+./kubectl.sh create -f postgres-pod-no-vol.yml
 ./kubectl.sh create -f postgres-service.yml
