c3067c5594877e05028debf04c8294c93eee3032
Stephen Sisk <sisk@google.com>
2017-04-21 17:23:02 -0700

    FileSystemRegistrar now allows throwing IOException

--- sdks/java/io/google-cloud-platform/src/test/java/org/apache/beam/sdk/io/gcp/storage/GcsFileSystemRegistrarTest.java
+++ sdks/java/io/google-cloud-platform/src/test/java/org/apache/beam/sdk/io/gcp/storage/GcsFileSystemRegistrarTest.java
@@ -22,6 +22,8 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import com.google.common.collect.Lists;
+
+import java.io.IOException;
 import java.util.ServiceLoader;
 
 import org.apache.beam.sdk.io.FileSystemRegistrar;
@@ -37,7 +39,7 @@ import org.junit.runners.JUnit4;
 public class GcsFileSystemRegistrarTest {
 
   @Test
-  public void testServiceLoader() {
+  public void testServiceLoader() throws IOException {
     for (FileSystemRegistrar registrar
         : Lists.newArrayList(ServiceLoader.load(FileSystemRegistrar.class).iterator())) {
       if (registrar instanceof GcsFileSystemRegistrar) {
